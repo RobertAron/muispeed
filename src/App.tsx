1386ceed182a1840d9014fcceb81523e33771505
Robert Aron <robert.aron.tx@gmail.com>
2022-06-04 16:22:23 -0500

    my changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,24 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 
+// Need to import from the full path, since that's the one thats being overwritten.
+import styled from "@mui/system/styled";
+
+const MyTypography = styled(Typography)(({theme})=>({
+  margin: theme.spacing(1)
+}));
+// Speed test.
+const test : number = 1 + 1
+
 function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
+    <MyTypography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}.
-    </Typography>
+    </MyTypography>
   );
 }
 
@@ -21,9 +30,11 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
+        {/* My type isn't perfectly accurate, so you'll sometimes wind up with some features that don't work */}
+        {/* I'm sure it can be improved on. I don't use those features in my own code though so I haven't put in the effort. */}
+        <MyTypography variant="h4" component="h1" gutterBottom>
           Create React App example with TypeScript
-        </Typography>
+        </MyTypography>
         <ProTip />
         <Copyright />
       </Box>
