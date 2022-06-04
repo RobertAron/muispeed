f9e5b298e27953d36d5f1ca69fa5b11432487f9e
Robert Aron <robert.aron.tx@gmail.com>
2022-06-04 16:31:28 -0500

    uncomment the styles

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,26 +1,26 @@
-// // # More performant styled function
+// # More performant styled function
 
-// import { Theme } from "@mui/material";
+import { Theme } from "@mui/material";
 
-// // ## Curried function to put styles in
-// type NestedSelector = React.CSSProperties | { [key: string]: NestedSelector | undefined }
-// type ExtendReactComponentProps<Component extends (...args: any) => JSX.Element, Additional> = (props: Parameters<Component>[0] & Additional) => ReturnType<Component>
-// interface FastCreateStyled<C> {
-//   (selectors: NestedSelector): C;
-//   <AdditionalProps extends {} = {}>(cb: (props: { theme: Theme } & AdditionalProps) => NestedSelector): ExtendReactComponentProps<C, AdditionalProps>
-// }
+// ## Curried function to put styles in
+type NestedSelector = React.CSSProperties | { [key: string]: NestedSelector | undefined }
+type ExtendReactComponentProps<Component extends (...args: any) => JSX.Element, Additional> = (props: Parameters<Component>[0] & Additional) => ReturnType<Component>
+interface FastCreateStyled<C> {
+  (selectors: NestedSelector): C;
+  <AdditionalProps extends {} = {}>(cb: (props: { theme: Theme } & AdditionalProps) => NestedSelector): ExtendReactComponentProps<C, AdditionalProps>
+}
 
-// // ## Function that creates the styling curry
-// interface Options {
-//   shouldForwardProp: (propKey: string) => boolean
-// }
-// interface MyStyled {
-//   <C extends keyof JSX.IntrinsicElements>(tag: C, options: Options = {}): FastCreateStyled<(props: JSX.IntrinsicElements[C]) => JSX.Element>
-//   <C extends React.JSXElementConstructor<React.ComponentProps<C>>>(component: C, options: Options = {}): FastCreateStyled<C>
-//   <C extends React.ComponentClass<React.ComponentProps<C>>>(component: C, options: Options = {}): FastCreateStyled<C>
-// }
-// // ## Can't export interfaces so assign it
-// let MyStyledFunction: MyStyled
-// declare module "@mui/system/styled" {
-//   export = MyStyledFunction
-// }
+// ## Function that creates the styling curry
+interface Options {
+  shouldForwardProp: (propKey: string) => boolean
+}
+interface MyStyled {
+  <C extends keyof JSX.IntrinsicElements>(tag: C, options: Options = {}): FastCreateStyled<(props: JSX.IntrinsicElements[C]) => JSX.Element>
+  <C extends React.JSXElementConstructor<React.ComponentProps<C>>>(component: C, options: Options = {}): FastCreateStyled<C>
+  <C extends React.ComponentClass<React.ComponentProps<C>>>(component: C, options: Options = {}): FastCreateStyled<C>
+}
+// ## Can't export interfaces so assign it
+let MyStyledFunction: MyStyled
+declare module "@mui/system/styled" {
+  export = MyStyledFunction
+}
